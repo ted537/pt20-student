2c8f328abedffda0705ba502220be093ecc7932e
Ted Brownlow <ted.brownlow@gmail.com>
2021-02-06 21:57:46 -0500

    renamed identifiers

--- test/scanner/test-one.sh
+++ test/scanner/test-one.sh
@@ -6,12 +6,6 @@
 
 test_case=$1
 
-failure() {
-    echo "FAIL: $test_case"
-    echo "$1"
-    exit 1
-}
-
 ptc_command="./bin/ptc -o1 -t1 -L lib/pt $test_case.pt"
 # write stdout to file, ignore stderr
 ssltrace "$ptc_command" lib/pt/scan.def -e \
@@ -20,7 +14,8 @@ ssltrace "$ptc_command" lib/pt/scan.def -e \
 
 # if .expected file does not exist
 if [ ! -f $test_case.expected ]; then 
-    failure "ERROR: expected file does not exist"
+    echo "FAIL: expected file does not exist"
+    exit 1
 fi
 
 diff=$(diff $test_case.expected $test_case.output)
@@ -29,5 +24,6 @@ diff=$(diff $test_case.expected $test_case.output)
 if [ -z "$diff" ]; then
     echo "PASS: $1"
 else
-    failure "$diff"
+    echo "FAIL: $test_case.expected does not match $test_case.output"
+    exit 1
 fi
