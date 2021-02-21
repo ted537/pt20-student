3404be5718844557d07be0dc093ca3fe02ff421a
Ted Brownlow <ted.brownlow@gmail.com>
2021-02-21 17:33:16 -0500

    introduced stripped comparison

--- test_all.py
+++ test_all.py
@@ -4,16 +4,22 @@ from subprocess import check_output, DEVNULL
 from functools import partial
 import pytest
 
+def striplines(text):
+    return [
+        line.strip()
+        for line in text.split('\n')
+    ]
+
+def compare_stripped(a,b):
+    return striplines(a) == striplines(b)
+
 def ssltracer(def_filename,o,t):
     def ssltrace(pt_filename):
         ptc_command = f'./bin/ptc -o{o} -t{t} -L lib/pt {pt_filename}'
         raw_trace = check_output([
             'ssltrace',ptc_command,def_filename,'-e',
         ], stderr=DEVNULL)
-        return '\n'.join(
-            line
-            for line in raw_trace.decode('ascii').split('\n')
-        )
+        return raw_trace.decode('ascii')
     return ssltrace
 
 scanner_ssltrace = ssltracer('lib/pt/scan.def',o=1,t=1)
@@ -27,9 +33,11 @@ def find_test_paths(root: PathLike):
 
 @pytest.mark.parametrize('path',find_test_paths('test/scanner'))
 def test_scanner(path):
-    scanner_trace = scanner_ssltrace(path+'.pt')
-    print(scanner_trace)
+    output = scanner_ssltrace(path+'.pt')
+    with open(path+'.expected') as file:
+        expected = file.read()
+    assert compare_stripped(output,expected)
 
 @pytest.mark.parametrize('path',find_test_paths('test/parser'))
 def test_parser(path):
-    pass
+    
