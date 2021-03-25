aeb57a3595c939e22b90a5d5c69f565ca30eb211
Ted Brownlow <ted.brownlow@gmail.com>
2021-03-25 14:41:59 -0400

    updated trace commands

--- test/coder-trace.sh
+++ test/coder-trace.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # bind trace to semantic parameters
 
+# generate assembly
+./test/trace.sh lib/pt/coder.def 4 4 .coder $1 -S
+# generate executable
 ./test/trace.sh lib/pt/coder.def 4 4 .coder $1
