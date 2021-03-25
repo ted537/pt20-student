11f5bb4b1aeb824325cf403604dd258c93227f35
Ted Brownlow <ted.brownlow@gmail.com>
2021-03-25 14:55:44 -0400

    removed coder trace file

--- test/coder-trace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# bind trace to semantic parameters
-
-# generate assembly
-./test/trace.sh lib/pt/coder.def 4 4 .coder $1 -S
-# generate executable
-./test/trace.sh lib/pt/coder.def 4 4 .coder $1
