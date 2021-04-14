9c5e2e22f278fa6a47e203c5f9916703d98714bf
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-14 11:58:01 -0400

    fixed compile script

--- test/compile-all.sh
+++ test/compile-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source_files=$(find test/coder -name "*.pt")
+source_files=$(find test/coder test/examples -name "*.pt")
 
 for source_file in $source_files; do
-    echo "Compiling $source_files"
-    ./test/compile.sh $source_file
+    echo "Compiling $source_file"
+    ./test/compile.sh $source_file > /dev/null
 done
